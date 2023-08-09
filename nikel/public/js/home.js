72c81aa784320332b50f437d0dd196b069523a88
luiza klein <112557450+rkluiza@users.noreply.github.com>
2023-08-08 23:59:26 -0300

    Update home.js

--- nikel/public/js/home.js
+++ nikel/public/js/home.js
@@ -1,17 +1,20 @@
 const myModal = new bootstrap.Modal("#transaction-modal");
 const session = localStorage.getItem("session");
 let logged = sessionStorage.getItem("logged");
+
+
 let data = {
     transactions: []
 };
 
 
+
 document.getElementById("button-logout").addEventListener("click", logout);
-checkLogged();
+
 
 //adicionar lancamento
 
-document.getElementById("transatcion-form").addEventListener("submit", function(e){
+document.getElementById("transaction-form").addEventListener("submit", function (e) {
     e.preventDefault();
 
     const value = parseFloat(document.getElementById("value-input").value);
@@ -28,35 +31,129 @@ document.getElementById("transatcion-form").addEventListener("submit", function(
     e.target.reset();
     myModal.hide();
     alert("Lançamento adicionado com sucesso!");
-    
+    getCashIn();
+    getCashOut()
 })
 
-function checkLogged(){
-    if(session){
+checkLogged();
+
+function checkLogged() {
+    if (session) {
         sessionStorage.setItem("logged", session);
         logged = session;
+
     }
 
-    if(!logged){
-        window.location.href="index.html"
+    if (!logged) {
+        window.location.href = "index.html"
+        return;
     }
 
     const dataUser = localStorage.getItem("logged");
-    if(dataUser){
+    if (dataUser) {
         data = JSON.parse(dataUser);
     }
 
-    console.log(data);
+    
 
 }
 
-function logout(){
+
+function logout() {
     sessionStorage.removeItem("logged");
     localStorage.removeItem("session");
 
-    window.location.href ="index.html"
+    window.location.href = "index.html"
 }
 
-function saveData(data){
-    localStorage.setItem(data.login, JSON.stringify(saveData));
+function getCashIn() {
+    const transactions = data.transactions;
+    const cashIn = transactions.filter((item) => item.type === "1");
+
+    let limit = 0;
+    let cashInhtml = ``;
+
+    if (cashIn.length) {
+        
+        limit = 0;
+    }
+
+    if (cashIn.length > 5) {
+        limit = 5;
+    } else {
+        limit = cashIn.length;
+    }
+
+    for (let index = 0; index < limit; index++) {
+        cashInhtml += `
+        <div class="row mb-4">
+        <div class="col-12">
+            <h3 class="fs-2">R$ ${cashIn[index].value.toFixed(2)}</h3>
+            <div class="container p-0">
+                <div class="row">
+                    <div class="col-12 col-md-8">
+                        <p>${cashIn[index].description}</p>
+                    </div>
+                    <div class="col-12 col-md-3 d-flex justify-content-end">
+                        ${cashIn[index].date}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        `
+
+    }
+
+
+    document.getElementById("cash-in-list").innerHTML = cashInhtml;
+
+}
+
+function getCashOut() {
+    const transactions = data.transactions;
+    const cashOut = transactions.filter((item) => item.type === "2");
+
+    let limit = 0;
+    let cashOuthtml = ``;
+
+    if (cashOut.length) {
+        
+        limit = 0;
+    }
+
+    if (cashOut.length > 5) {
+        limit = 5;
+    } else {
+        limit = cashOut.length;
+    }
+
+    for (let index = 0; index < limit; index++) {
+        cashOuthtml += `
+        <div class="row mb-4">
+        <div class="col-12">
+            <h3 class="fs-2">R$ ${cashOut[index].value.toFixed(2)}</h3>
+            <div class="container p-0">
+                <div class="row">
+                    <div class="col-12 col-md-8">
+                        <p>${cashOut[index].description}</p>
+                    </div>
+                    <div class="col-12 col-md-3 d-flex justify-content-end">
+                        ${cashOut[index].date}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        `
+
+    }
+
+
+    document.getElementById("cash-out-list").innerHTML = cashOuthtml;
+
+}
+
+function saveData(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
 }
